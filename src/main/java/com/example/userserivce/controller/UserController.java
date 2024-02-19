78c2378a78fe298088f77d4bdaba06f5ac72fcfb
jmcunst <xnslqjtmghaf@gmail.com>
2024-02-19 16:13:45 +0900

    update : gateway url

--- src/main/java/com/example/userserivce/controller/UserController.java
+++ src/main/java/com/example/userserivce/controller/UserController.java
@@ -28,9 +28,9 @@ public class UserController {
     }
 
 
-    @GetMapping("/health-check")
+    @GetMapping("/user-service/health-check")
     public String status(){
-        return "It's Working in User Service";
+        return String.format("It's Working in User Service on PORT %s", env.getProperty("local.server.port"));
     }
 
     @GetMapping("/welcome")
