6a4406802fcf6319f82dfdc8e452c97218c9fbc0
jmcunst <xnslqjtmghaf@gmail.com>
2024-02-20 10:14:45 +0900

    feat : auth filter

--- src/main/java/com/example/userserivce/security/WebSecurity.java
+++ src/main/java/com/example/userserivce/security/WebSecurity.java
@@ -5,15 +5,22 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authorization.AuthorizationDecision;
+import org.springframework.security.config.annotation.ObjectPostProcessor;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationFilter;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+import org.springframework.security.web.util.matcher.IpAddressMatcher;
+
+import java.util.function.Supplier;
 
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
@@ -21,68 +28,57 @@ import static org.springframework.security.web.util.matcher.AntPathRequestMatche
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurity {
+    private static final String ALLOWED_IP_ADDRESS = "192.168.0.30";
+    private static final String SUBNET = "/32";
+    private static final IpAddressMatcher ALLOWED_IP_ADDRESS_MATCHER = new IpAddressMatcher(ALLOWED_IP_ADDRESS + SUBNET);
+    private static final String IP_CHECK_PATH_PREFIX = "/api/temp";
+    private static final String IP_CHECK_PATH_PATTERN = IP_CHECK_PATH_PREFIX + "/**";
+    private static final String[] WHITE_LIST = {
+            "/welcome",
+            "/health-check",
+            "/users"
+    };
+
+    private final UserService userService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final Environment environment;
+    private final ObjectPostProcessor<Object> objectPostProcessor;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.csrf(AbstractHttpConfigurer::disable)
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         request -> {
-                            request.requestMatchers(antMatcher("**")).permitAll();
-                            request.requestMatchers(antMatcher("/users/**")).permitAll();
+                            request.requestMatchers(WHITE_LIST).permitAll();
+                            request.requestMatchers(IP_CHECK_PATH_PATTERN).access(this::hasIpAddress)
+                                    .anyRequest().authenticated();
                         }
-                ).headers(header -> header.frameOptions(
-                        HeadersConfigurer.FrameOptionsConfig::disable
-                ));
+                )
+                .headers(header -> header.frameOptions(
+                                HeadersConfigurer.FrameOptionsConfig::disable
+                        )
+                ).addFilter(getAuthenticationFilter());
 
         return http.build();
     }
-    // 1안
-//    private static final String[] WHITE_LIST = {
-//            "/users/**",
-//            "/**"
-//    };
-//
-//    @Bean
-//    protected SecurityFilterChain config(HttpSecurity http) throws Exception {
-//        http.csrf().disable();
-//        http.headers().frameOptions().disable();
-//        http.authorizeHttpRequests(authorize -> authorize
-//                .requestMatchers(WHITE_LIST).permitAll());
-//        return http.build();
-//    }
-//    최종안
-//    private final UserService userService;
-//    private final BCryptPasswordEncoder bCryptPasswordEncoder;
-//    private final Environment environment;
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.csrf(AbstractHttpConfigurer::disable)
-//                .authorizeHttpRequests(request -> {
-//                    request.requestMatchers(antMatcher("/actuator/**")).permitAll();
-//                    request.requestMatchers(antMatcher("/**")).permitAll();
-//                })
-//                //            .headers(header -> header.frameOptions(
-//                //                frameOptionsConfig -> frameOptionsConfig.disable()))
-//                .apply(new MyCustomSecurity());
-//
-//        return http.build();
-//    }
-//
-//    public class MyCustomSecurity extends AbstractHttpConfigurer<MyCustomSecurity, HttpSecurity> {
-//
-//        @Override
-//        public void configure(HttpSecurity http) throws Exception {
-//
-//            AuthenticationManager authenticationManager = http.getSharedObject(
-//                    AuthenticationManager.class);
-//            AuthenticationFilter authenticationFilter = new AuthenticationFilter(authenticationManager, userService, environment);
-//            http.addFilter(authenticationFilter);
-//        }
-//
-//        protected void configure2(AuthenticationManagerBuilder auth) throws Exception {
-//            auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
-//        }
-//
-//    }
+
+    public AuthenticationManager authenticationManager(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+        return auth.build();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        AuthenticationManagerBuilder builder = new AuthenticationManagerBuilder(objectPostProcessor);
+        authenticationFilter.setAuthenticationManager(authenticationManager(builder));
+        return authenticationFilter;
+    }
+
+    private AuthorizationDecision hasIpAddress(Supplier<Authentication> authentication, RequestAuthorizationContext object) {
+        return new AuthorizationDecision(
+                !(authentication.get() instanceof AnonymousAuthenticationToken)
+                        && ALLOWED_IP_ADDRESS_MATCHER.matches(object.getRequest()
+                ));
+    }
 }
